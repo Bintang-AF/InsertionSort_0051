4171776c588688bab36340e82899dcb667fbf716
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-03-26 13:20:09 +0700

    prosedur pengulangan

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -25,4 +25,9 @@ void input()
     cout << "masukkan element array" << endl;
     cout << "=======================" << endl;
 
+    for (int i = 0; i < n; i++)//menggunakan pengulangan for untuk menyimpan data pada array
+    {
+        cout << "data ke-" << (i + 1) << ": ";//memasukkan atau menginputkan nilai data n
+        cin >> arr[i]; //menyimpan nilai data n kedalam array arr
+    }
 }
