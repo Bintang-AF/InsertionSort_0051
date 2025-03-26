fca4426ee8d9c6a57b6c92b3cdcef9ea7bfa6408
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-03-26 13:04:35 +0700

    prosedur input

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -3,3 +3,21 @@ Using namespace std;
 
 int arr[20]; //membuat array dengan panjang data 20
 int n; // membuat variable inputan n
+
+void input()
+{ // produce input
+    while (true)
+    {
+        cout << "masukkan jumlah data pada array : "; //membuat inputan jumlah element array
+        cin >> n; // memanggil variable inputan n
+
+        if  (n <= 20)
+        {// membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\narray yang anda masukkan maksimal 20 elemen.\n";//menampilkan pesan jika data lebih dari 20
+        }
+    }
+}
