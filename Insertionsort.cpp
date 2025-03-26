e9f967d8245a63549c5e2a4b073d2c332820b66f
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-03-26 13:30:54 +0700

    prosedur insertionsort

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -31,3 +31,27 @@ void input()
         cin >> arr[i]; //menyimpan nilai data n kedalam array arr
     }
 }
+
+void insertionsort()
+{//procedure insertionsort
+    
+int temp; //membuat variable data temporer atau penyimpanan sementara
+int j, i; //membuat variable j sebagai penanda
+
+for (i = 1; i <= n - 1; i++)
+{// step 1
+    
+temp = arr[i]; //step 2
+
+j = i - 1; //step 3
+
+while  (j >= 0 && arr[j] > temp)//step 4
+{
+    arr[j + 1] = temp ; //step 4a
+    j--; //step 4b
+}
+
+arr[j + 1] = temp; //step 5
+}
+}
+ 
